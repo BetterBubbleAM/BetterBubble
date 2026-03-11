e48449f7dfd26ecb26acff7b41b03dee0f32ea97
BetterBubbleAM <legendakekee@gmail.com>
2026-03-11 01:45:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,42 +8,40 @@ app.use(express.static(__dirname + '/public'));
 let players = {};
 let food = [];
 
-// Generujemy jedzenie na dużej mapie
-for(let i=0; i<200; i++) {
+for(let i=0; i<150; i++) {
     food.push({
-        x: Math.random() * 3000, 
-        y: Math.random() * 3000, 
+        x: Math.random() * 2000, 
+        y: Math.random() * 2000, 
         color: `hsl(${Math.random() * 360}, 100%, 50%)`
     });
 }
 
 io.on('connection', (socket) => {
-    // Tworzymy gracza od razu po wejściu
     players[socket.id] = {
-        x: 1500,
-        y: 1500,
-        size: 25,
-        color: `hsl(${Math.random() * 360}, 100%, 50%)`
+        x: 1000, y: 1000, size: 25,
+        color: `hsl(${Math.random() * 360}, 100%, 50%)`,
+        targetX: 1000, targetY: 1000
     };
 
     socket.on('movement', (data) => {
         if(players[socket.id]) {
-            // Płynny ruch w stronę myszki
-            players[socket.id].x += (data.x - players[socket.id].x) * 0.1;
-            players[socket.id].y += (data.y - players[socket.id].y) * 0.1;
+            players[socket.id].targetX = data.x;
+            players[socket.id].targetY = data.y;
         }
     });
 
-    socket.on('disconnect', () => {
-        delete players[socket.id];
-    });
+    socket.on('disconnect', () => { delete players[socket.id]; });
 });
 
+// Silnik serwera - płynne przesuwanie pozycji na serwerze
 setInterval(() => {
+    for(let id in players) {
+        let p = players[id];
+        p.x += (p.targetX - p.x) * 0.15;
+        p.y += (p.targetY - p.y) * 0.15;
+    }
     io.emit('state', { players, food });
-}, 1000 / 60);
+}, 1000 / 30); // 30 FPS wystarczy dla płynności przy interpolacji
 
 const PORT = process.env.PORT || 10000;
-http.listen(PORT, '0.0.0.0', () => {
-    console.log('Serwer BetterBubble działa na porcie ' + PORT);
-});
+http.listen(PORT, '0.0.0.0');
